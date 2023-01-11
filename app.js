527f5189dee14e3cacf9eb4be3948612cecbe280
unknown <fabriciovics@gmail.com>
2023-01-11 19:37:01 -0300

    Corrigindo app js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 console.log("Rodando o sistema de cadastro de jogos")
 console.log("Iniciando conexão com o banco de dados")
-<<<<<<< HEAD
-
-=======
-
->>>>>>> 20ff7cdcb4311c7574d16614ff3dabf1e80fc4e1
+//corrigindo
